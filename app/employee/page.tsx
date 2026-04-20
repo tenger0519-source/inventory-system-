77d291cb2d53210a8af07c151e7b33872662c122
tenger0519-source <tenger0519@gmail.com>
2026-04-20 11:01:12 +0800

    chnage

--- app/employee/page.tsx
+++ app/employee/page.tsx
@@ -96,24 +96,6 @@ export default function EmployeeHome() {
           </CardContent>
         </Card>
 
-        {/* ===== POSITIONS ===== */}
-        <Card>
-          <CardHeader>
-            <CardTitle>Барааны позициид</CardTitle>
-            <CardDescription>
-              Барааны төрөл, санхүүгийн секториор хайж үзэх
-            </CardDescription>
-          </CardHeader>
-
-          <CardContent>
-            <Link href="/employee/positions">
-              <Button className="w-full">
-                Барааны позициид
-              </Button>
-            </Link>
-          </CardContent>
-        </Card>
-
         {/* ===== LOCATIONS ===== */}
         <Card>
           <CardHeader>
@@ -131,6 +113,7 @@ export default function EmployeeHome() {
             </Link>
           </CardContent>
         </Card>
+
 
         {/* ===== WEEKLY GRAPH ===== */}
         <Card>
