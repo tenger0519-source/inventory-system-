f98ea6191c0ecc1a5062acf0c4b2fe64f31caedd
tenger0519-source <tenger0519@gmail.com>
2026-04-21 11:33:54 +0800

    change

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,23 +1,3 @@
-"use client"
-
-import { useEffect, useState } from "react"
-import dynamic from "next/dynamic"
-
-const DashboardComponent = dynamic(() => import('./dashboard-component'), {
-  ssr: false,
-  loading: () => <div>Loading...</div>
-})
-
 export default function DashboardPage() {
-  const [mounted, setMounted] = useState(false)
-
-  useEffect(() => {
-    setMounted(true)
-  }, [])
-
-  if (!mounted) {
-    return <div>Loading...</div>
-  }
-
-  return <DashboardComponent />
+  return null
 }
